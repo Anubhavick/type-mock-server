2594ec77b04bcf0cb44f56bd5e3bc73227bae69a
Anubhav Mishra <anubhav.ickk@gmail.com>
2025-08-11 20:45:34 +0530

    fix: Update CLI version to match package version 1.0.2

--- src/cli.ts
+++ src/cli.ts
@@ -8,7 +8,7 @@ const program = new Command();
 program
   .name('type-mock-server')
   .description('Type-driven mock API generator')
-  .version('1.0.0');
+  .version('1.0.1');
 
 program
   .command('start')
